e4a1b857b3063497ef536a92d18772c424b55d27
Julien DESCORPS <julien.descorps@telecom-sudparis.eu>
2015-05-13 13:03:22 +0200

    indexAction + Suppression AppBundle

--- projet/src/OC/QuizgenBundle/Controller/DefaultController.php
+++ projet/src/OC/QuizgenBundle/Controller/DefaultController.php
@@ -12,33 +12,44 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class DefaultController extends Controller
 {
     public function indexAction()
-    {
-        return $this->render('OCQuizgenBundle:Default:index.html.twig');
-    }
-     public function addAction(Request $request){
-     // On crée un objet Quiz
-    $quiz = new Quiz();
-    // On crée le FormBuilder grâce au service form factory
-    $form = $this->get('form.factory')->create(new QuizType(), $quiz);
-    // On ajoute les champs de l'entité que l'on veut à notre formulaire
-    
-    // À partir de maintenant, la variable $quiz contient les valeurs entrées dans le formulaire par le visiteur
-    if ($form->handleRequest($request)->isValid()) {
-      $em = $this->getDoctrine()->getManager();
-      $em->persist($quiz);
-      $em->flush();
+	{	
+		$listQuizs = $this
+			->getDoctrine()
+			->getManager()
+			->getRepository('OCQuizgenBundle:Quiz')
+			->findAll()
+		;
 
-      $request->getSession()->getFlashBag()->add('notice', 'Vous avez créé un quiz avec succès.');
+		return $this->render('OCQuizgenBundle:Default:index.html.twig', array(
+		  'listQuizs' => $listQuizs
+		));
+	}
+	
+	public function addAction(Request $request){
+	
+		 // On crée un objet Quiz
+		$quiz = new Quiz();
+		// On crée le FormBuilder grâce au service form factory
+		$form = $this->get('form.factory')->create(new QuizType(), $quiz);
+		// On ajoute les champs de l'entité que l'on veut à notre formulaire
+		
+		// À partir de maintenant, la variable $quiz contient les valeurs entrées dans le formulaire par le visiteur
+		if ($form->handleRequest($request)->isValid()) {
+		  $em = $this->getDoctrine()->getManager();
+		  $em->persist($quiz);
+		  $em->flush();
 
-      return $this->redirect($this->generateUrl('oc_quizgen_view', array('id' => $quiz->getId())));
-    }
-         
-    // On passe la méthode createView() du formulaire à la vue
-    // afin qu'elle puisse afficher le formulaire toute seule
-    return $this->render('OCQuizgenBundle:Default:add.html.twig', array(
-      'form' => $form->createView(),
-    ));
-  } 
+		  $request->getSession()->getFlashBag()->add('notice', 'Vous avez créé un quiz avec succès.');
+
+		  return $this->redirect($this->generateUrl('oc_quizgen_view', array('id' => $quiz->getId())));
+		}
+			 
+		// On passe la méthode createView() du formulaire à la vue
+		// afin qu'elle puisse afficher le formulaire toute seule
+		return $this->render('OCQuizgenBundle:Default:add.html.twig', array(
+		  'form' => $form->createView(),
+		));
+	} 
   
   public function viewAction($id)
   {
