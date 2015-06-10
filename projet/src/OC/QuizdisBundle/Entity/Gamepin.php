b55f3d3c66933133962895382d497452b519bbf3
rinj <rindra.andriamanefo@telecom-sudparis.eu>
2015-06-10 14:12:22 +0200

    Erreur quand on rentre un gamepin qui n'existe pas (création d'un gamepintype, de validators)

---
+++ projet/src/OC/QuizdisBundle/Entity/Gamepin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace OC\QuizdisBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use OC\QuizdisBundle\Validator as OCQuizdisAssert;
+
+/**
+ * Gamepin
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="OC\QuizdisBundle\Entity\Gamepin")
+ * @OCQuizdisAssert\VerifGamepinExists
+ */
+class Gamepin
+{
+ 
+	/**
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+	
+	/**
+     * @ORM\Column(type="integer")
+     */
+    private $gamepin;
+		
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set gamepin
+     *
+     * @param integer $gamepin
+     * @return ReponseQuestion
+     */
+    public function setGamepin($gamepin)
+    {
+        $this->gamepin = $gamepin;
+
+        return $this;
+    }
+
+    /**
+     * Get gamepin
+     *
+     * @return integer 
+     */
+    public function getGamepin()
+    {
+        return $this->gamepin;
+    }
+
+}
