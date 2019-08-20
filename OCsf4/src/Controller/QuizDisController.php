7ac89b042d7ff0612ddb06a64dc7d7f1e7175191
Christian Bac <christian.bac@telecom-sudparis.eu>
2019-08-20 16:30:21 +0200

    Templates Layouts uniformisation

--- OCsf4/src/Controller/QuizDisController.php
+++ OCsf4/src/Controller/QuizDisController.php
@@ -18,10 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-/**
- *
- * @Route("/quizdis")
- */
+
 class QuizDisController extends AbstractController
 {
 
